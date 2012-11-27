f334ea7d2f938cb881d7a087ebe79307117aed0b
Charl <charl@blazingchilli.com>
2012-11-27 15:45:22 +0200

    Update AssemblyCompany

--- MuninNodeDotNet/Properties/AssemblyInfo.cs
+++ MuninNodeDotNet/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("MuninNode.Net")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Blazingchilli Ltd")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MuninNode.Net")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
