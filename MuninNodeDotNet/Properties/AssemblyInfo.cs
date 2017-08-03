e0b378cc7fb648c52710e1a572922580e5da8566
Charl Möller <cwmoller@sun.ac.za>
2017-08-03 15:59:39 +0200

    Convert to VS2017, replace InstallShield

--- MuninNodeDotNet/Properties/AssemblyInfo.cs
+++ MuninNodeDotNet/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MuninNode.Net")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyCopyright("Released under MIT license")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.2012.1128")]
-[assembly: AssemblyFileVersion("1.0.2012.1128")]
+[assembly: AssemblyVersion("1.0.2017.0803")]
+[assembly: AssemblyFileVersion("1.0.2017.0803")]
